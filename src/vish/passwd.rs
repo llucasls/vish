3b9093a9fc1adc9c25afd9b82fab5b905b381603
Lucas L. S. Haine <lucaslshaine@gmail.com>
2024-06-03 12:32:05 -0300

    refactor: return None on error
    The get_home function now returns None when it fails to create either a
    CString or a CStr, instead of panicking.

--- src/vish/passwd.rs
+++ src/vish/passwd.rs
@@ -6,14 +6,14 @@ extern "C" {
 }
 
 pub fn get_home(user: String) -> Option<String> {
-    let username = CString::new(user).expect("CString::new failed");
+    let username = CString::new(user).ok()?;
     unsafe {
         let home_dir_ptr = get_home_dir(username.as_ptr());
 
         if home_dir_ptr.is_null() {
             None
         } else {
-            let home_dir = CStr::from_ptr(home_dir_ptr).to_str().expect("CStr::from_ptr failed");
+            let home_dir = CStr::from_ptr(home_dir_ptr).to_str().ok()?;
             Some(String::from(home_dir))
         }
     }
