3642b62552c674575958c2b0dab84340928bd164
Lucas L. S. Haine <lucaslshaine@gmail.com>
2024-06-02 22:12:17 -0300

    refactor: turn program into module

--- get_home.c
+++ get_home.c
@@ -1,25 +1,11 @@
-#include <stdio.h>
+#include <unistd.h>
 #include <pwd.h>
-#include <errno.h>
 
-// get-home <user>
-// retrieve user's home directory full path
+char *get_home_dir(char *username) {
+    struct passwd *pwd = getpwnam(username);
 
-int main(int argc, char *argv[]) {
-    errno = 0;
-    if (argc == 1)
-        return 1;
+    if (pwd == NULL)
+        return NULL;
 
-    struct passwd *pwd = getpwnam(argv[1]);
-
-    if (pwd == NULL) {
-        fprintf(stderr, "Error: can't retrieve user's home directory.\n");
-        return 1;
-    }
-
-    printf("home: %s\n", pwd->pw_dir);
-
-    if (errno != 0)
-        printf("errno: %d\n", errno);
-    return 0;
+    return pwd->pw_dir;
 }
