6fc6ecef0a20d20253e03dae80987c8463236a8c
Lucas L. S. Haine <lucaslshaine@gmail.com>
2025-04-04 18:25:46 -0300

    style: change type into Self
    Change every reference to the Field type into Self for consistency.

--- src/vish/string/field.rs
+++ src/vish/string/field.rs
@@ -109,68 +109,68 @@ fn get_parameter_name(text: String) -> (String, String) {
 }
 
 impl Field<String> {
-    pub fn new(text: String) -> Field<String> {
+    pub fn new(text: String) -> Self {
         let size: usize = text.len();
 
         if ! text.starts_with('$') {
-            Field::Plain(text)
+            Self::Plain(text)
         } else if is_special_parameter(&text) {
             let name = text.chars().nth(1);
             if name.is_none() {
-                return Field::Plain(String::new());
+                return Self::Plain(String::new());
             }
             let mut par = String::new();
             par.push(name.unwrap());
-            Field::Special(par)
+            Self::Special(par)
         } else if is_unenclosed_parameter(&text) {
             let (par, _rest) = get_parameter_name(String::from(&text[1..]));
-            Field::Parameter(par)
+            Self::Parameter(par)
         } else if in_braces(&text) && !is_number(&text) {
             let par = String::from(&text[2..size - 1]);
-            Field::Parameter(par)
+            Self::Parameter(par)
         } else if in_braces(&text) && is_number(&text) {
             let num = String::from(text[2..size - 1].trim_start_matches('0'));
-            Field::Position(num)
+            Self::Position(num)
         } else if in_single_parenthesis(&text) {
             let command = String::from(&text[2..size - 1]);
-            Field::Command(command)
+            Self::Command(command)
         } else if in_backticks(&text) {
             let command = String::from(&text[1..size - 1]);
-            Field::Command(command)
+            Self::Command(command)
         } else if in_double_parenthesis(&text) {
             let math = String::from(&text[3..size - 2]);
-            Field::Arithmetic(math)
+            Self::Arithmetic(math)
         } else if in_single_quotes(&text) {
             let text = String::from(&text[1..size - 1]);
-            Field::Quoted(text)
+            Self::Quoted(text)
         } else {
-            Field::Plain(text)
+            Self::Plain(text)
         }
     }
 
     pub fn into_inner(self) -> String {
         match self {
-            Field::Plain(text) => text,
-            Field::Parameter(name) => name,
-            Field::Command(command) => command,
-            Field::Arithmetic(operation) => operation,
-            Field::Quoted(text) => text,
-            Field::CStyleQuoted(text) => text,
-            Field::Position(number) => number,
-            Field::Special(character) => character,
+            Self::Plain(text) => text,
+            Self::Parameter(name) => name,
+            Self::Command(command) => command,
+            Self::Arithmetic(operation) => operation,
+            Self::Quoted(text) => text,
+            Self::CStyleQuoted(text) => text,
+            Self::Position(number) => number,
+            Self::Special(character) => character,
         }
     }
 
     pub fn substitute(self) -> String {
         match &self {
-            Field::Plain(text) => text.to_string(),
-            Field::Parameter(_) => self.substitute_parameter(),
-            Field::Command(text) => format!("command: {}", text),
-            Field::Arithmetic(text) => format!("arithmetic: {}", text),
-            Field::Quoted(text) => format!("quoted: {}", text),
-            Field::CStyleQuoted(text) => format!("quoted: {}", text),
-            Field::Position(text) => format!("positional parameter: {}", text),
-            Field::Special(text) => format!("special parameter: {}", text),
+            Self::Plain(text) => text.to_string(),
+            Self::Parameter(_) => self.substitute_parameter(),
+            Self::Command(text) => format!("command: {}", text),
+            Self::Arithmetic(text) => format!("arithmetic: {}", text),
+            Self::Quoted(text) => format!("quoted: {}", text),
+            Self::CStyleQuoted(text) => format!("quoted: {}", text),
+            Self::Position(text) => format!("positional parameter: {}", text),
+            Self::Special(text) => format!("special parameter: {}", text),
         }
     }
 
